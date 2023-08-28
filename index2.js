f5684499c450113fade4466c28843ce1f6205d05
Aman <aman.brikkin@gmail.com>
2023-08-28 17:18:57 +0530

    comment

--- index2.js
+++ index2.js
@@ -45,27 +45,27 @@ app.listen(8080, () => {
     console.log("Hey, now listen on port 8080!")
 });
 
-import { createCluster } from 'redis';
+// import { createCluster } from 'redis';
 
-const cluster = createCluster({
-    rootNodes: [
-        {
-            url: 'redis://127.0.0.1:16379'
-        },
-        {
-            url: 'redis://127.0.0.1:16380'
-        },
-        // ...
-    ]
-});
+// const cluster = createCluster({
+//     rootNodes: [
+//         {
+//             url: 'redis://127.0.0.1:16379'
+//         },
+//         {
+//             url: 'redis://127.0.0.1:16380'
+//         },
+//         // ...
+//     ]
+// });
 
-cluster.on('error', (err) => console.log('Redis Cluster Error', err));
+// cluster.on('error', (err) => console.log('Redis Cluster Error', err));
 
-await cluster.connect();
+// await cluster.connect();
 
-await cluster.set('foo', 'bar');
-const value = await cluster.get('foo');
-console.log(value); // returns 'bar'
+// await cluster.set('foo', 'bar');
+// const value = await cluster.get('foo');
+// console.log(value); // returns 'bar'
 
-await cluster.quit();
+// await cluster.quit();
 
