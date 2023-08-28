3611535ecfaa5f07b0e0e9b7bec4bfe9b86f2fac
Aman <aman.brikkin@gmail.com>
2023-08-28 15:49:08 +0530

    connection and send data from post

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// import express, { json } from 'express';
-// import { createClient } from "redis";
-// import { promisify } from "util";
-// const redisUrl = "redis://127.0.0.1:6380"
-// const client = createClient(redisUrl)
+import express, { json } from 'express';
+import { createClient } from "redis";
+import { promisify } from "util";
+const redisUrl = "redis://127.0.0.1:6380"
+const client = createClient(redisUrl)
 
-// client.set = promisify(client.set)
+client.set = promisify(client.set)
 
-// const app = express()
+const app = express()
 
-// app.use(json())
+app.use(json())
 
-// app.post("/", async (req, res) => {
-//     // res.json({ msg: "hi" })
-//     const { key, value } = req.body
-//     const response = await client.set(key, value)
-//     res.json(response)
-// })
+app.post("/", async (req, res) => {
+    // res.json({ msg: "hi" })
+    const { key, value } = req.body
+    const response = await client.set(key, value)
+    res.json(response)
+})
 
 
-// app.listen(8080, () => {
-//     console.log("Hey, now listen on port 8080!")
-// });
+app.listen(8080, () => {
+    console.log("Hey, now listen on port 8080!")
+});
 
 // client.connect().catch(console.error)
